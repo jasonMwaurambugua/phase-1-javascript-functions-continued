6dc54134e4aa9a1bf2ff4c6bd13b83d74c2df076
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-24 16:27:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// code your solution here
+//javascript-functions-continued
+//jason
+// my solution here
+
+function saturdayFun() {
+    console.log("This Satuday I want to attend Ruby Hack at Moringa School");
+    saturdayFun();
+    
+}
+
+function saturdayFun() {
+    console.log("This Satuday I want to roller-scate");
+    saturdayFun();
+    
+}
+
+function saturdayFun() {
+    console.log("This Satuday I want to bathe my dog");
+    saturdayFun();   
+}
+
+function saturdayFun() {
+    console.log("This Satuday I want to bathe my dog");
+    saturdayFun();
+}
+
+function mondayWork() {
+    console.log("This Monday,i will work-out");
+    mondayWork();
+}
+
+function mondayWork() {
+    console.log("This Monday,i will go to the office");
+    mondayWork();
+}
+
+function mondayWork() {
+    console.log("This Monday,i will work from home");
+    mondayWork();
+}
+
+function wrapAdjective() {
+    const part1 = "a hard worker";
+    return function () {
+      const part2 = "you are";
+      return function () {
+        console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+      };
+    };
+  }
+  
+  function wrapAdjective() {
+    const part1 = "You are";
+    return function () {
+      const part2 = "a dedicated proggrammer";
+      return function () {
+        console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+      };
+    };
+  }
+  
+  
+
+
+
+
